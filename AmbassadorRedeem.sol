98201cb33dc08c3351d70ceb1aa910c4cc7c02f1
eason <>
2023-05-07 00:19:36 +0800

    rewrite using import and add swap pot fee

--- AmbassadorRedeem.sol
+++ AmbassadorRedeem.sol
@@ -6,12 +6,12 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract AmbassadorRedeem is Ownable {
+contract AmbassadorRedeem is Ownable, Pausable {
     using SafeERC20 for IERC20;
     using ECDSA for bytes32;
 
-    bool public Pause = false;
     IERC20 public USDT = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
     address private _signerAddress = 0x865073D81e144987dDEB4d810704ffF4182fF897;
     mapping(bytes32 => bool) public _swapKey;
@@ -21,12 +21,18 @@ contract AmbassadorRedeem is Ownable {
     constructor(){
     }
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
     function SetUSDT(address _address) external onlyOwner{
         USDT = IERC20(_address);
     }
-    function SetPause(bool _status) external onlyOwner{
-        Pause = _status;
-    }
+
     function SetSigner(address _address) external onlyOwner{
         _signerAddress = _address;
     }
@@ -35,8 +41,7 @@ contract AmbassadorRedeem is Ownable {
         string calldata timestamp_,
         bytes calldata signature_,
         uint amount_
-    ) external {
-        require(Pause == false, "Contract is paused");
+    ) external whenNotPaused {
         require(amount_ > 0, "Invalid amount");
         bytes32 message =  getMessage(timestamp_, amount_, msg.sender);
         require(!_swapKey[message], "Key Already Claimed");
--- Liquidity_Wallet.sol
+++ Liquidity_Wallet.sol
@@ -1,426 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity ^0.8.16;
 
-// File: @openzeppelin/contracts/math/Math.sol
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-/**
- * @dev Standard math utilities missing in the Solidity language.
- */
-library Math {
-    /**
-     * @dev Returns the largest of two numbers.
-     */
-    function max(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a >= b ? a : b;
-    }
-
-    /**
-     * @dev Returns the smallest of two numbers.
-     */
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
-    /**
-     * @dev Returns the average of two numbers. The result is rounded towards
-     * zero.
-     */
-    function average(uint256 a, uint256 b) internal pure returns (uint256) {
-        // (a + b) / 2 can overflow, so we distribute
-        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
-    }
+interface IERC20Wrapper is IERC20 {
+    function burn(uint256 amount) external;
 }
 
-// File: @openzeppelin/contracts/GSN/Context.sol
-
-/*
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with GSN meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-contract Context {
-    // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, which should be used via inheritance.
-    constructor () { }
-    // solhint-disable-previous-line no-empty-blocks
-
-    function _msgSender() internal view returns (address payable) {
-        return payable(msg.sender);
-    }
-
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-// File: @openzeppelin/contracts/ownership/Ownable.sol
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () {
-        _owner = _msgSender();
-        emit OwnershipTransferred(address(0), _owner);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
- */
-interface IERC20 {
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `recipient`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function mint(address account, uint amount) external;
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `sender` to `recipient` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function burn(uint256 amount) external returns (bool);
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-// File: @openzeppelin/contracts/utils/Address.sol
-
-/**
- * @dev Collection of functions related to the address type
- */
-library Address {
-    /**
-     * @dev Returns true if `account` is a contract.
-     *
-     * This test is non-exhaustive, and there may be false-negatives: during the
-     * execution of a contract's constructor, its address will be reported as
-     * not containing a contract.
-     *
-     * IMPORTANT: It is unsafe to assume that an address for which this
-     * function returns false is an externally-owned account (EOA) and not a
-     * contract.
-     */
-    function isContract(address account) internal view returns (bool) {
-        // This method relies in extcodesize, which returns 0 for contracts in
-        // construction, since the code is only stored at the end of the
-        // constructor execution.
-
-        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
-        // for accounts without code, i.e. `keccak256('')`
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != 0x0 && codehash != accountHash);
-    }
-
-    /**
-     * @dev Converts an `address` into `address payable`. Note that this is
-     * simply a type cast: the actual underlying value is not changed.
-     *
-     * Available since v2.4.0.
-     */
-    function toPayable(address account) internal pure returns (address payable) {
-        return payable(address(uint160(account)));
-    }
-
-    /**
-     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
-     * `recipient`, forwarding all available gas and reverting on errors.
-     *
-     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
-     * of certain opcodes, possibly making contracts go over the 2300 gas limit
-     * imposed by `transfer`, making them unable to receive funds via
-     * `transfer`. {sendValue} removes this limitation.
-     *
-     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
-     *
-     * IMPORTANT: because control is transferred to `recipient`, care must be
-     * taken to not create reentrancy vulnerabilities. Consider using
-     * {ReentrancyGuard} or the
-     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
-     *
-     * Available since v2.4.0.
-     */
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
-        // solhint-disable-next-line avoid-call-value
-        // (bool success, ) = recipient.call.value(amount)("");
-        (bool success, ) = recipient.call{value:amount}("");
-        require(success, "Address: unable to send value, recipient may have reverted");
-    }
-}
-
-
-// File: contractsStrings.sol
-
-
-library Strings {
-  // via https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
-  function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal pure returns (string memory) {
-      bytes memory _ba = bytes(_a);
-      bytes memory _bb = bytes(_b);
-      bytes memory _bc = bytes(_c);
-      bytes memory _bd = bytes(_d);
-      bytes memory _be = bytes(_e);
-      string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-      bytes memory babcde = bytes(abcde);
-      uint k = 0;
-      for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-      for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-      for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-      for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-      for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-      return string(babcde);
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, _d, "");
-    }
-
-    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
-        return strConcat(_a, _b, _c, "", "");
-    }
-
-    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
-        return strConcat(_a, _b, "", "", "");
-    }
-
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = bytes1(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-
-    /**
-     * Index Of
-     *
-     * Locates and returns the position of a character within a string
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string acting as the haystack to be
-     *              searched
-     * @param _value The needle to search for, at present this is currently
-     *               limited to one character
-     * @return int The position of the needle starting from 0 and returning -1
-     *             in the case of no matches found
-     */
-    function indexOf(string memory _base, string memory _value)
-        internal
-        pure
-        returns (int) {
-        return _indexOf(_base, _value, 0);
-    }
-
-    /**
-     * Index Of
-     *
-     * Locates and returns the position of a character within a string starting
-     * from a defined offset
-     * 
-     * @param _base When being used for a data type this is the extended object
-     *              otherwise this is the string acting as the haystack to be
-     *              searched
-     * @param _value The needle to search for, at present this is currently
-     *               limited to one character
-     * @param _offset The starting point to start searching from which can start
-     *                from 0, but must not exceed the length of the string
-     * @return int The position of the needle starting from 0 and returning -1
-     *             in the case of no matches found
-     */
-    function _indexOf(string memory _base, string memory _value, uint _offset)
-        internal
-        pure
-        returns (int) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
-
-        assert(_valueBytes.length == 1);
-
-        for (uint i = _offset; i < _baseBytes.length; i++) {
-            if (_baseBytes[i] == _valueBytes[0]) {
-                return int(i);
-            }
-        }
-
-        return -1;
-    }
-}
-
-// File: @openzeppelin/contracts/token/ERC20/SafeERC20.sol
-
-
-contract  Liquidity_Wallet is Ownable{
+contract  Liquidity_Wallet is Ownable, Pausable {
     using Address for address;
+    using SafeERC20 for IERC20Wrapper;
     
-    bool public Pause = false;
     uint public UserIdCounter = 0;
     uint public Rate = 100;
     uint public Decimal = 10 ** 18;
+    uint public Fee = 0; // min=1(0.01%) max=10000(100%)
+    address public FeeRecipient = address(0);
 
-    IERC20 public USDT = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
-    IERC20 public POT = IERC20(0xa7e5F7471D673fCcd38606FCd6c3dCbdE807D95c);
-    
+    IERC20Wrapper public USDT = IERC20Wrapper(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
+    IERC20Wrapper public POT = IERC20Wrapper(0xa7e5F7471D673fCcd38606FCd6c3dCbdE807D95c);
+
     address public gov;
 
-    event RedeemedAmount(address indexed _address, uint indexed _amount);
+    event RedeemedAmount(address indexed _address, uint indexed _amount, uint indexed _returnAmount);
     
     constructor(){
         gov = msg.sender;
     }
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
     function SetUSDT(address _address) external onlyOwner{
-        USDT = IERC20(_address);
+        USDT = IERC20Wrapper(_address);
     }
     function SetPOT(address _address) external onlyOwner{
-        POT = IERC20(_address);
-    }
-    function SetPause(bool _status) external onlyOwner{
-        Pause = _status;
+        POT = IERC20Wrapper(_address);
     }
     function SetRate(uint _rate) external{
         require(msg.sender == gov, "!gov");
@@ -433,20 +60,37 @@ contract  Liquidity_Wallet is Ownable{
     function SetGov(address _address) external onlyOwner{
         gov = _address;
     }
+    function SetFee(uint _fee) external{
+        require(msg.sender == gov, "!gov");
+        require(_fee >= 0 && _fee <= 10000, "fee range: 1 - 10000");
+        Fee = _fee;
+    }
+    function SetFeeRecipient(address _feeRecipient) external{
+        require(msg.sender == gov, "!gov");
+        FeeRecipient = _feeRecipient;
+    }
 
     function Claim(
         uint _amount
-    ) external returns (uint){
-        require(Pause == false, "Contract is paused");
+    ) external whenNotPaused returns (uint){
         require(_amount > 0, "Invalid fund");
 
-        POT.transferFrom(msg.sender, address(this), _amount);
+        POT.safeTransferFrom(msg.sender, address(this), _amount);
 
         uint returnAmount = Convert(_amount);
         require(returnAmount > 0, "Invalid Conversion");
-        emit RedeemedAmount(msg.sender, returnAmount);
-        USDT.transfer(msg.sender, returnAmount);
+
+        // transfer fee USDT to FeeRecipient
+        if (Fee > 0 && FeeRecipient != address(0)) {
+            uint feeAmount = returnAmount * Fee / 10000;
+            returnAmount = returnAmount - feeAmount;
+            USDT.safeTransfer(FeeRecipient, feeAmount);
+        }
+
+        USDT.safeTransfer(msg.sender, returnAmount);
         POT.burn(_amount);
+        emit RedeemedAmount(msg.sender, _amount, returnAmount);
+
         return returnAmount;
     }
 
@@ -456,8 +100,6 @@ contract  Liquidity_Wallet is Ownable{
     }
 
     function Save(address _token, uint _amount) external onlyOwner{
-        IERC20(_token).transfer(msg.sender, _amount);
+        IERC20Wrapper(_token).safeTransfer(msg.sender, _amount);
     }
 }
-
-
